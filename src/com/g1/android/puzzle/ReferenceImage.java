8c3d89a77f361beee535309207eb409ec1602dcb
Anil <amadamala@gmail.com>
2011-02-12 01:49:01 +0530

    First import

---
+++ src/com/g1/android/puzzle/ReferenceImage.java
@@ -0,0 +1,12 @@
+package com.g1.android.puzzle;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ReferenceImage extends Activity{
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.image);
+	}
+}
